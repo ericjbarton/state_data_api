33e769da946fd217b948672c306c6e391b0f6c29
ericjbarton <erockbart@gmail.com>
2022-10-09 14:01:00 -0400

    adds states index, states abbreivation show routes, adds states controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'states/index'
+  get "/states" => "states#index"
+  get "/states/:STATE_ABBREVIATION" => "states#show"
 end
