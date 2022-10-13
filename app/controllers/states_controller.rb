13566172d6f59a725779d7a972578aec483a00e6
ericjbarton <erockbart@gmail.com>
2022-10-13 19:14:14 -0400

    safety commit

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,11 +1,17 @@
 class StatesController < ApplicationController
   def index
-    @states = State.all
-    render json: @states.all
+    states = State.all
+    render json: states.all
   end
 
   def show
-    @state = State.find_by(code: params[:code].upcase)
-    render json: @state
+    state_id = params["id"]
+    @state = State.find_by(code: params["code"].upcase) || @state = State.find_by(state: params["state"])
+    render json: @state.as_json
   end
+
+  # def show_state
+  #   @state = State.find_by(state: params[:state])
+  #   render json: @state
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/states" => "states#index"
   get "/states/:code" => "states#show"
+  get "/states/:state" => "states#show_state"
 end
