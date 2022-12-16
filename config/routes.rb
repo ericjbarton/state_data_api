f7721b946e497b15b5c2643ecb25ed13628dcd95
ericjbarton <erockbart@gmail.com>
2022-12-16 15:12:52 -0500

    fixes it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/states" => "states#index"
   get "/states/:code" => "states#show"
-  get "/states/:state" => "states#show_state"
-  get "/states/codes" => "states#code_index"
+  # get "/states/:state" => "states#show_state"
+  # get "/states/codes" => "states#code_index"
 end
